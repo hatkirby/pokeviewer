353b170392892a1f9bdde74550112b5131c99fd2
Kelly Rauchenberger <fefferburbia@gmail.com>
2017-10-03 22:49:19 -0400

    Optimized front page queries
    Reduced the number of queries that the front page runs to 2 queries
    total: one to get trainer data, and one to get the Pokémon. The latter
    query selects only the necessary columns, notably ignoring basically
    everything from the revisions join other than the nickname of the most
    recent revision. The controller then does the work of chunking the
    Pokémon into party/boxes, and then into trainers/unaccounted.
    
    refs #2

--- app/controllers/pokeviewer/pokemon_controller.rb
+++ app/controllers/pokeviewer/pokemon_controller.rb
@@ -3,12 +3,73 @@ require_dependency "pokeviewer/application_controller"
 module Pokeviewer
   class PokemonController < ApplicationController
     def index
-      @trainers = Trainer.all
-      @unaccounted = Pokemon.where(trainer: nil)
+      pokemon = Pokemon.joins(:revisions).
+        order("trainer_id IS NULL DESC").
+        order(trainer_id: :asc).
+        order(box: :asc).
+        order(slot: :asc).
+        order("pokeviewer_revisions.sequential_id DESC").
+        group("pokeviewer_pokemon.uuid").
+        select(:box, :slot, :uuid, :trainer_id, :species_id).
+        select(:ot_gender, :ot_name, :unown_letter).
+        select("pokeviewer_revisions.nickname AS nickname").
+        chunk do |p|
+          if p.trainer_id.nil?
+            -1
+          else
+            p.trainer_id
+          end
+        end
+
+      if pokemon.peek.first == -1
+        @unaccounted = pokemon.next.second
+      else
+        @unaccounted = []
+      end
+
+      @trainers = Trainer.order(id: :asc).all.map do |trainer|
+        if trainer.id == pokemon.peek.first
+          party = []
+
+          boxes = (1..14).map do |i|
+            {
+              name: trainer.box_name(i),
+              pokemon: [nil] * 30
+            }
+          end
+
+          pokemon.next.second.chunk do |p|
+            if p.box.nil?
+              -1
+            else
+              p.box
+            end
+          end.each do |box, pokes|
+            if box == -1
+              party = pokes
+            else
+              boxes[box-1][:pokemon] = (0..29).map do |i|
+                if not pokes.empty? and (pokes.first.slot == i)
+                  pokes.shift
+                else
+                  nil
+                end
+              end
+            end
+          end
+
+          [trainer, party, boxes]
+        else
+          nil
+        end
+      end.compact
     end
 
     def show
-      @pokemon = Pokemon.find_by_uuid! params[:id]
+      @pokemon = Pokemon.includes(
+          :trainer, :species, :location,
+          revisions: [:item, :move_1, :move_2, :move_3, :move_4]
+        ).find_by_uuid! params[:id]
     end
   end
 end
